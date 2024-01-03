1f733c8edb688f01ae331de33a91c4d30e55df02
Ethan Franson <ethan.r.franson@gmail.com>
2024-01-02 21:19:41 -0700

    updating pizza menu project

--- src/index.js
+++ src/index.js
@@ -1,17 +1,47 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import pizzaData from './data'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+function App(){
+    return (
+        <div>
+            <Header/>
+            <Menu />
+            <Footer/>
+        </div>
+    )
+} 
+function Header() {
+    return <h1>Fast React Pizza Co.</h1>
+}
+
+function Menu() {
+    return (
+        <div>
+            <h1>Our Menu</h1>
+            <Pizza />
+        </div>
+    )
+}
+
+function Footer() {
+    return <footer>{new Date().toLocaleTimeString()} We are currently open.</footer>
+}
+
+function Pizza(){
+    return (
+        <div>
+            <img src={pizzaData[1].photoName} alt={pizzaData[1].name}/>
+            <h2>{pizzaData[1].name}</h2>
+            <p>{pizzaData[1].ingredients }</p>
+        </div>
+    )
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <React.StrictMode>
+        <App/>
+    </React.StrictMode>
+)
